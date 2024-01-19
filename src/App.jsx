c6a498e2bfda1b2371a511b2dfef7af089addf16
MarcosPerez16 <marcos.perez06@yahoo.com>
2024-01-19 03:39:30 -0800

    workshop completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,43 @@
+import { useState } from "react";
+import { puppyList } from "./data";
+import "./App.css";
+
 function App() {
-  return <div>Test</div>;
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  console.log("puppyList: ", puppyList);
+
+  function handleClick() {}
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
+  return (
+    <div className='App'>
+      <div>
+        <p>{featPupId}</p>
+        {featPupId && (
+          <div>
+            <h2 style={{ color: "red" }}>{featuredPup.name}</h2>
+            <ul>
+              <li style={{ color: "yellow" }}>Age: {featuredPup.age}</li>
+              <li style={{ color: "orange" }}>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
+      </div>
+      {puppies.map((puppy) => {
+        return (
+          <p
+            onClick={() => {
+              setFeatPupId(puppy.id);
+            }}
+            key={puppy.id}
+          >
+            {puppy.name}
+          </p>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
